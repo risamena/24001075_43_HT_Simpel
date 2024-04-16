573609aed960d7b3eba856b25686610e5b6a1663
risamena <hannytuhuteru@gmail.com>
2024-04-16 21:09:56 +0900

    add morgan for error handling and nodemon start script

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
+const PORT = 4646
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// morgan
+// internal server error handler
+app.use( function(err, req, res, next){
+    console.log(err);
+    res.status(500).json({
+        status: 'fail',
+        errors: err.message
+    });
+
+});
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        errors: 'Maaf, halaman tidak ditemukan!'
+    });
+});
+
+// server
+app.listen(PORT, () => { console.log(`Server is running at Port ${PORT}`);});
