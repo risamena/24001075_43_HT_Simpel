6c3412eb69618cd0eba633e42c716dd39161b35b
risamena <hannytuhuteru@gmail.com>
2024-05-05 23:06:01 +0900

    add postgree client & env

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 // config postgree: baiknya di file lain
-
+const pg = require('pg');
+const client = new pg.Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+});
+client.connect((error) => {
+    if (error) {console.log('Error: ', error);} else {console.log('connected to database');};
+});
+// client.end();
 
 // morgan
 // internal server error handler
